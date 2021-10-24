0cab78228e5ae97a094cfa25e8430279e120c60f
Lourdes Aguirre <loumaagui@gmail.com>
2021-10-23 18:32:53 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 import { app } from './app';
-import { initializeDatabaseSystem } from './database';
+import { initializeDatabaseSystem } from './model/database';
 
 (() => {
   dotenv.config();
   app.listen(app.get('port'), () =>
-    console.log(`Server on port:${app.get('port')}} `)
+    console.log(`Server on port:${app.get('port')} `)
   );
   initializeDatabaseSystem();
 })();
